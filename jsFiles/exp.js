443048f529a27a3fea1910fd2c55dbf52ae8fd3a
Paul Stillman <paulstillman@users.noreply.github.com>
2023-05-24 16:13:58 -0400

    deleted cache

--- jsFiles/exp.js
+++ jsFiles/exp.js
@@ -10,4 +10,25 @@ const welcome = {
 
 timeline.push(welcome);
 
-jsPsych.run(timeline);
+const instructions = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+      <p>In this experiment, a circle will appear in the center 
+      of the screen.</p><p>If the circle is <strong>blue</strong>, 
+      press the letter F on the keyboard as fast as you can.</p>
+      <p>If the circle is <strong>orange</strong>, press the letter J 
+      as fast as you can.</p>
+      <div style='width: 700px;'>
+      <div style='float: left;'><img src='img/blue.png'></img>
+      <p class='small'><strong>Press the F key</strong></p></div>
+      <div style='float: right;'><img src='img/orange.png'></img>
+      <p class='small'><strong>Press the J key</strong></p></div>
+      </div>
+      <p>Press any key to begin.</p>
+    `,
+    post_trial_gap: 2000
+  };
+  
+  timeline.push(instructions);
+
+  jsPsych.run(timeline);
